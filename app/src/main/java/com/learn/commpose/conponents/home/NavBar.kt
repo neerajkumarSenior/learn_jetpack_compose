f64b4aab89b66f54c2dd3adffaeb5909b82d00d2
neerajkumarSenior <neerajkumarmaurya@hotmail.com>
2024-09-30 01:50:58 +0530

    add drawer and toolbar

---
+++ app/src/main/java/com/learn/commpose/conponents/home/NavBar.kt
@@ -0,0 +1,9 @@
+package com.learn.commpose.conponents.home
+
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class NavBarItem(
+    
+    val label:String,
+    val icon:ImageVector
+)
